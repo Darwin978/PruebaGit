bb4da46a6a5565b67727539f3aec73ee0cd5fca2
Darwin Gonzalez <darwin.gonzalez.est@tecazuay.edu.ec>
2022-01-24 18:50:10 -0500

    primeros cambios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlumnoComponent } from './alumno/alumno.component';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlumnoComponent,
+    InicioComponent,
+    LoginComponent,
+    CabeceraComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
